351b37940ff68b84caa1280348b88737d6c35960
regggulus <regggulus@gmail.com>
2024-12-27 23:20:55 +0300

    Counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
 import './App.css';
 
 function App() {
+    const maxValue = 5
+    const minValue = 0
+    const [count, setCount] = useState(minValue)
     return (
-        <div>
-<div>new Counter</div>
+        <div className={'App'}>
+            {count}
+            <button onClick={()=> {setCount(count+1)}}>int</button>
+            <button onClick={()=> {setCount(minValue)}}>reset</button>
         </div>
     )
 }
