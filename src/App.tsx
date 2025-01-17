adb2c8abe0e0a145eecb7f61149d864b0fb450dd
regggulus <regggulus@gmail.com>
2025-01-17 17:39:52 +0300

    backlighting of the counter button when the value is reached

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,15 @@ function App() {
     const maxValue = 5
     const minValue = 0
     const [count, setCount] = useState(minValue)
+    const countColorWhite = {color: 'white'}
+    const countColorRed = {color: 'red'}
     return (
         <div className={'App'}>
             Счетчик:
-            {count}
+            <div style={count >= maxValue ? countColorRed : countColorWhite}>
+                {count}
+            </div>
+
             <div>
                 <button style={{}} onClick={()=> {setCount( count+1)}} disabled={count>=maxValue}>int</button>
                 <button onClick={()=> {setCount(minValue)}} disabled={count < maxValue}>reset</button>
